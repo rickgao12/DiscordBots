1755aaacf778e41ae242949c47816253d554e7b1
dalongerlong 1 <rgao3736@gmail.com>
2020-05-29 20:45:34 -0500

    Added comma-number package

--- cmds/covid.js
+++ cmds/covid.js
@@ -16,12 +16,12 @@ module.exports.run = async (bot, message, command, args) => {
             const embed = new Discord.MessageEmbed()
                 .setTitle('Updated COVID-19 Numbers')
                 .setColor(0xeb3434)
-                .addField('New Confirmed: ', Global.NewConfirmed)
-                .addField('Total Confirmed: ', Global.TotalConfirmed)
-                .addField('New Deaths: ', Global.NewDeaths)
-                .addField('Total deaths:', Global.TotalDeaths)
-                .addField('New Recovered: ', Global.NewRecovered)
-                .addField('Total recovered: ', Global.TotalRecovered)
+                .addField('New Confirmed: ', commaNumber(Global.NewConfirmed))
+                .addField('Total Confirmed: ', commaNumber(Global.TotalConfirmed))
+                .addField('New Deaths: ', commaNumber(Global.NewDeaths))
+                .addField('Total deaths:', commaNumber(Global.TotalDeaths))
+                .addField('New Recovered: ', commaNumber(Global.NewRecovered))
+                .addField('Total recovered: ', commaNumber(Global.TotalRecovered))
 
             message.channel.sendEmbed(embed);
 
@@ -40,12 +40,12 @@ module.exports.run = async (bot, message, command, args) => {
                 .setColor('#0099ff')
                 .setTitle(findCountry.Country)
                 .setThumbnail('https://i.imgur.com/LBnCThp.png')
-                .addField('New Confirmed: ', findCountry.NewConfirmed)
-                .addField('Total Confirmed: ', findCountry.TotalConfirmed)
-                .addField('New Deaths: ', findCountry.NewDeaths)
-                .addField('Total deaths:', findCountry.TotalDeaths)
-                .addField('New Recovered: ', findCountry.NewRecovered)
-                .addField('Total recovered: ', findCountry.TotalRecovered)
+                .addField('New Confirmed: ', commaNumber(findCountry.NewConfirmed))
+                .addField('Total Confirmed: ', commaNumber(findCountry.TotalConfirmed))
+                .addField('New Deaths: ', commaNumber(findCountry.NewDeaths))
+                .addField('Total deaths:', commaNumber(findCountry.TotalDeaths))
+                .addField('New Recovered: ', commaNumber(findCountry.NewRecovered))
+                .addField('Total recovered: ', commaNumber(findCountry.TotalRecovered))
 
             message.channel.send(embed);
         }
